be0457caafdf062bcde1eb236e67f643ef118b61
DeVPSman <git@devpsman.nl>
2015-04-14 13:21:24 +0200

    modified:   config.inc 	modified:   functions.inc 	modified:   siteBackup.bash

--- siteBackup.bash
+++ siteBackup.bash
@@ -1,12 +1,49 @@
 #!/bin/bash
 
+export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+
 . config.inc
 . functions.inc
 
 TIMESTRING=`date +%F-%H%M`
-webdirs=`getWebdirectories`
 
-for site in $webdirs; do
+# tests
+if ! [ -d "${BACKUPDIR}" ]; then
+	echo "backup dir ${BACKUPDIR} bestaat niet!"
+	mkdir ${BACKUPDIR};
+fi
+
+if ! [ -d "${DBDUMPDIR}" ]; then
+	echo "Database backup dir ${DBDUMPDIR} bestaat niet!"
+	mkdir ${DBDUMPDIR};
+fi
+
+cd ${WEBSERVER_ROOT}
+
+for site in `getWebdirectories`; do
 	# tarren
-	$TARBIN czfv ${BACKUPDIR}/${site}.${TIMESTRING}.tgz ${WEBSERVER_ROOT}/${site}
+	backupFile="${BACKUPDIR}/${site}.${TIMESTRING}.tgz"
+	echo "Making backup: ${backupFile}"
+	$TARBIN czf ${backupFile} ${site}
+	deleteList=`getDeletelist ${BACKUPDIR}/${site}`
+	for deleteBackup in $deleteList; do
+		echo "deleting backup: $deleteBackup"
+		rm ${deleteBackup}
+	done
 done
+
+for db in `getDatabases`; do
+	backupFile="${DBDUMPDIR}/${db}.${TIMESTRING}.sql.gz"
+	echo "Making database dump: $backupFile"
+	$MYSQLDUMP -cnl ${db} | gzip -5 > ${backupFile}
+	deleteList=`getDeletelist ${DBDUMPDIR}/${db}`
+	for deleteBackup in $deleteList; do
+		echo "deleting database backup: $deleteBackup"
+		rm ${deleteBackup}
+	done
+done
+
+
+
+
+
